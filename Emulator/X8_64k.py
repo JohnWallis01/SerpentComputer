17774e0cd4a4d72e0ce63a2f373ac03675ecebe4
John Wallis <eviper01@gmail.com>
2021-08-16 17:19:02 +0800

    fixing mem differences

--- Emulator/X8_64k.py
+++ Emulator/X8_64k.py
@@ -118,8 +118,8 @@ def MEO():
 
 
 def SMA():
-    global RamAddr
-    RamAddr = Main_Reg
+    global RamAddrLow
+    RamAddrLow = Main_Reg
 
 
 def FLF():
@@ -221,8 +221,8 @@ def run():
         instruct = '{0:{fill}{width}b}'.format((x + 2**n) % 2**n, fill='0', width=n)
         decode(instruct)
         executed += 1
-        print("c", Counter, "ins", instruct, "main", Main_Reg)
-        time.sleep(0.2)
+        # print("c", Counter, "ins", instruct, "main", Main_Reg, "mem addr", RamAddrLow, "value", RAM[RamAddrLow])
+        # time.sleep(0.2)
 
 
 run()
